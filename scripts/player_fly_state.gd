51ca077946095a131e22bffb68cd4978c501fece
Dcarreon <dcarreonr@gmail.com>
2024-07-18 12:04:35 -0500

    set the game loop functions to false until state is entered

--- scripts/player_fly_state.gd
+++ scripts/player_fly_state.gd
@@ -1,13 +1,18 @@
 extends "res://scripts/state.gd"
 
+@export var player: CharacterBody2D
+
 func _ready() -> void:
-	pass 
+	set_process(false)
+	set_physics_process(false)
 
 func _process(delta: float) -> void:
-	pass
+	player.move_and_collide(player.velocity, delta)
 
 func _enter_state() -> void:
-	pass
+	set_process(true)
+	set_physics_process(true)
 
 func _exit_state() -> void:
-	pass
+	set_process(false)
+	set_physics_process(false)
