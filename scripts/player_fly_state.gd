cad74553ae0087e0236caa08636f6061b856bb23
Dcarreon <dcarreonr@gmail.com>
2024-07-18 11:55:10 -0500

    created the script for the fly state and attached it to it's node

---
+++ scripts/player_fly_state.gd
@@ -0,0 +1,13 @@
+extends "res://scripts/state.gd"
+
+func _ready() -> void:
+	pass 
+
+func _process(delta: float) -> void:
+	pass
+
+func _enter_state() -> void:
+	pass
+
+func _exit_state() -> void:
+	pass
