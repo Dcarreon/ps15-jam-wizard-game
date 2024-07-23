9dcabd3f4adad35038d08397643bc0b7dce64435
Dcarreon <dcarreonr@gmail.com>
2024-07-23 17:20:28 -0500

    deleted prototype level scene and renamed it's script to polygonal collision

--- scripts/level/polygonal_collision.gd
+++ scripts/level/polygonal_collision.gd
@@ -1,7 +1,6 @@
 extends StaticBody2D
 
 @onready var line : Path2D = $Path2D
-@onready var shape : Polygon2D = $Polygon2D
 @onready var collision : CollisionPolygon2D = $CollisionPolygon2D
 
 # Called when the node enters the scene tree for the first time.
@@ -9,4 +8,3 @@ func _ready() -> void:
 	var points : PackedVector2Array = line.curve.get_baked_points()
 
 	collision.polygon = points
-	shape.polygon = points
