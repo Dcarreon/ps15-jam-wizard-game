6e63a90c0a6516b0e1114761fbf68451d78ed817
Dcarreon <dcarreonr@gmail.com>
2024-07-23 16:47:47 -0500

    implemented Path2D node to create smooth curves

--- scripts/level/level.gd
+++ scripts/level/level.gd
@@ -1,8 +1,12 @@
 extends StaticBody2D
 
+@onready var line : Path2D = $Path2D
 @onready var shape : Polygon2D = $Polygon2D
 @onready var collision : CollisionPolygon2D = $CollisionPolygon2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	collision.polygon = shape.polygon
+	var points : PackedVector2Array = line.get_baked_points()
+
+	collision.polygon = points
+	shape.polygon = points
