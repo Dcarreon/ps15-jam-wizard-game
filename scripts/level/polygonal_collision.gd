72c340334ca90d29e41465ef44d575309cd4d2d4
Dcarreon <dcarreonr@gmail.com>
2024-07-24 15:12:20 -0500

    changed collision variable name to collision object

--- scripts/level/polygonal_collision.gd
+++ scripts/level/polygonal_collision.gd
@@ -1,10 +1,11 @@
 extends StaticBody2D
 
 @onready var line : Path2D = $Path2D
-@onready var collision : CollisionPolygon2D = $CollisionPolygon2D
+@onready var collision_object : CollisionPolygon2D = $CollisionPolygon2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	add_to_group("static_collisions")
 	var points : PackedVector2Array = line.curve.get_baked_points()
 
-	collision.polygon = points
+	collision_object.polygon = points
