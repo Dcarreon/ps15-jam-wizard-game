5a063bb6255c0cc4f28fca1e426ab6f7c13168c1
Dcarreon <dcarreonr@gmail.com>
2024-07-24 15:48:54 -0500

    Created a falling area scene and its script

---
+++ scripts/level/falling_area.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var line : Path2D = $Path2D
+@onready var collision_object : CollisionPolygon2D = $CollisionPolygon2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var points : PackedVector2Array = line.curve.get_baked_points()
+
+	collision_object.polygon = points
