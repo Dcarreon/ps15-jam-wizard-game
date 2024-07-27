6e7b060f6ed9262af305409b593ee0b149b9275d
Diego Pavan <psdiegops@gmail.com>
2024-07-27 19:17:08 +0100

    old enemy region

--- scripts/enemy/navigation_region.gd
+++ scripts/enemy/navigation_region.gd
@@ -5,7 +5,8 @@ extends NavigationRegion2D
 func _ready() -> void:
 	navigation_polygon.agent_radius = 50
 	bake_navigation_polygon()
-
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
